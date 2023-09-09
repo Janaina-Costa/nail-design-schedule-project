38a4f34cc91a9bc3672fbc2f14690f44b0cb8546
Janaina-Costa <janainacosta.dev@gmail.com>
2023-09-09 19:05:11 -0300

    ajusta tabela de preços

--- script/tablePrice.js
+++ script/tablePrice.js
@@ -1,12 +1,30 @@
 const tableValues = {
-  firstAplication: 110,
-  maintenance: 90,
+    fiberGlass:{
+      name:'Fibra de Vidro',
+      firstAplication: 110,
+      maintenance: 90,
+    },
+    acrygel:{
+      name:'Acrigel',
+      firstAplication: 100,
+      maintenance: 85,
+    },
+    acrylic:{
+      name:'Acrílico',
+      firstAplication: 130,
+      maintenance: 110,
+    },
+    porcelain:{
+      name:'Porcelana',
+      firstAplication: 160,
+      maintenance: 125,
+    },
 
-  enameling: {
-    simple: 20,
-    gel: 35,
-    decoration: 30,
-  },
-};
+    enameling:{
+        simple:20,
+        gel:35,
+        decoration:30
+    }
+}
 
-export default tableValues;
+export default tableValues
