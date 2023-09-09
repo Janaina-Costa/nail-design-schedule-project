3462d4de81f42e4b44002938adae35685c804498
Janaina-Costa <janainacosta.dev@gmail.com>
2023-09-09 00:35:36 -0300

    estiliza menu de usuário cria link de agendamento flutuante

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
-const iconMenuMobile = document.querySelector('.menu-list-mobile')
-const dropdownMenu = document.querySelector('.dropdown-menu')
-const closeMenu = document.querySelector('.close-button')
+const setStyleByEvent = (component, event, child, propStyle) => {
+  component.addEventListener(event, () => {
+    child.style.display = propStyle
+  })
+}
 
-iconMenuMobile.addEventListener('click',(e)=>{
-  dropdownMenu.style.display= 'block'
-})
+const applicationContainer = document.querySelector('.container')
 
-closeMenu.addEventListener('click', ()=>{
-  dropdownMenu.style.display = 'none'
-  console.log('clique');
-})
+/*menu mobile */
+
+const toggleDropdownMenu = () => {
+  const iconMenuMobile = document.querySelector('.menu-list-mobile')
+  const dropdownMenu = document.querySelector('.dropdown-menu')
+  const closeMenu = document.querySelector('.close-button')
+
+  setStyleByEvent(iconMenuMobile, 'click', dropdownMenu, 'block')
+  setStyleByEvent(closeMenu, 'click', dropdownMenu, 'none')
+
+}
+toggleDropdownMenu()
+
+
+/*menu profile */
+
+
+const actionMenuProfile = () => {
+  const menuProfile = document.querySelector('.menu-profile')
+  const componentMenuProfile = document.querySelector('.wrapper-menu-profile')
+  const closeBtn = document.querySelector('.close')
+
+  setStyleByEvent(menuProfile, 'mouseover', componentMenuProfile, 'block')
+  setStyleByEvent(closeBtn, 'click', componentMenuProfile, 'none')
+ 
+}
+actionMenuProfile()
+
+
+const showFloatingLink =()=>{
+  const itemFloat = document.querySelector('.float')
+  const linkFloat = document.querySelector('.float p')
+  document.addEventListener('scroll',()=>{
+    let position = window.pageYOffset
+    console.log(position);
+    if(position > 0){
+      itemFloat.classList.add('open-float')
+  
+    }else{
+      itemFloat.classList.add('close-float')
+    }
+  })
+  
+  linkFloat.addEventListener('click', ()=>{
+    window.location.assign('http://127.0.0.1:5500/scheduling.html')
+  })
+
+}
+showFloatingLink()
+
+
+
+
+/**
+ * se estiver logado aparecer 
+ * o nome 
+ * o bairro
+ * meu perfil
+ * 
+ * se não estiver logado
+ * cadastre-se
+ * fazer login
+ * 
+ */
